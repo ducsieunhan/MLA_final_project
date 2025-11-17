52476ec25c6b2c5178a5e6e16bb69b9aabe8d2d1
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-11-17 17:51:44 +0700

    Update neural_network.py

--- code_starter/starter/neural_network.py
+++ code_starter/starter/neural_network.py
@@ -1,6 +1,6 @@
 # MLA Fall 2025 - Hanoi University
 # Academic Integrity Declaration:
-# I, [Student Name] ([Student ID]), declare that this code is my own original work.
+# I, Pham Thi Khanh Ngoc (2201040133), declare that this code is my own original work.
 # I have not copied or adapted code from any external repositories or previous years.
 # Any sources or libraries used are explicitly cited below.
 
